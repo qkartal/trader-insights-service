0d31ceb78d1692921678c24f1de4ce594712c8f8
Quinn Kartal <qkartal@bleepbleep.org.uk>
2017-04-22 16:05:56 +0000

    Red Hat

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -39,7 +39,7 @@ public class HttpApplication extends AbstractVerticle {
   private void greeting(RoutingContext rc) {
     String name = rc.request().getParam("name");
     if (name == null) {
-      name = "World";
+      name = "Red Hat";
     }
 
     JsonObject response = new JsonObject()
